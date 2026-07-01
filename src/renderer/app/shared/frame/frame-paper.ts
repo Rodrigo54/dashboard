db2ba196986fde9fadecc7bfc145aedf151a0e51
Rodrigo Alves <rodrigo54mix@gmail.com>
2026-07-01 17:43:11 -0300

    💄 style(frame-paper): ajusta classe CSS para o componente de frame

--- src/renderer/app/shared/frame/frame-paper.ts
+++ src/renderer/app/shared/frame/frame-paper.ts
@@ -5,7 +5,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   imports: [],
   template: `
     <div
-      class="w-full h-full rounded-md bg-card flex items-center justify-between p-6 shadow shadow-foreground/35 overflow-auto"
+      class="w-full h-full rounded-md block bg-card p-6 shadow shadow-foreground/35 overflow-auto"
     >
       <ng-content></ng-content>
     </div>
